70d0a08ecc253ca69548267d72317b76d08f44e2
Sino Samateh <sinosamateh@gmail.com>
2025-01-07 16:30:45 +0100

    fixed grid

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -13,7 +13,7 @@ const Services = () => {
         experience in multiple companies like ..... , .....
       </p>
 
-      <div className="grid grid-cols-4 gap-6 my-10">
+      <div className="grid grid-cols-auto gap-6 my-10">
         {serviceData.map(({ icon, title, description, link }, index) => (
           <div
             className="border border-gray-400 rounded-lg px-8 py-12 hover:shadow-black cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500"
